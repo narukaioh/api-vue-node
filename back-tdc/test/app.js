7c7e98fedaeac414e4c957ebd0d0872f58d29f90
jucieny <jucieny.santos@f1solucoes.com.br>
2017-10-19 17:22:31 -0200

    Add - eslint and other dependencies

---
+++ back-tdc/test/app.js
@@ -0,0 +1,10 @@
+
+const chai = require('chai');
+
+describe('testando', () => {
+    describe("primeiro teste", () => {
+        it('deve retornar verdadeiro', () => {
+            chai.should(1);
+        })
+    });
+});
