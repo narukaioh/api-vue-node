f4761c3855ed8615b7c0762876a0357a597ef665
Ju Dantas <jucienyds@gmail.com>
2017-10-22 23:07:45 -0200

    Adicionando serviço mongodb e rota para pokemons

--- back-tdc/src/server/index.js
+++ back-tdc/src/server/index.js
@@ -1,6 +1,7 @@
 
 const restify = require('restify')
 const server = restify.createServer()
+const pokeroutes = require('../http/pokeroutes')
 const routes = require('../http/routes')
 const cors = require('./cors')
 
@@ -9,5 +10,6 @@ server.use(cors.actual)
 server.use(restify.plugins.bodyParser())
 
 routes(server)
+pokeroutes(server)
 
 module.exports = server
