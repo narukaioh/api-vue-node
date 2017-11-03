cf2222df210aba892ecab0270c0eb16312962afd
jucieny <jucieny.santos@f1solucoes.com.br>
2017-11-03 17:13:53 -0200

    Fazendo ajustes na conexao com o mongo

--- src/services/mongodb/index.js
+++ src/services/mongodb/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
+const connection = mongoose.createConnection(process.env.MONGODB_URI, { useMongoClient: true })
+
+connection.then(function(db){
+  console.log(db)
+})
 
 const errorHandler = (error, msg, rejectFunction) => {
   console.error(error)
